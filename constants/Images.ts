fc936fdd9674a0d8e70fc4e87881774d4565e1a3
Keisuke Nemura <nemuraystk@gmail.com>
2023-11-11 19:02:45 +0900

    add AboutCard実装

---
+++ constants/Images.ts
@@ -0,0 +1,7 @@
+import { ImageSourcePropType } from 'react-native';
+
+const Images: Record<string, ImageSourcePropType> = {
+  icon_negate: require('../assets/images/icon_negate.jpeg'),
+};
+
+export default Images;
