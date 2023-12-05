1b1043db49ea1a7559380b6fea5fbf041caeaba8
森一貴 <morikazuki@morikazutakanoMacBook-Air.local>
2023-12-05 22:54:03 +0900

    feature: ArrowButton実装

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,13 +1,27 @@
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Alert } from 'react-native';
 
 import { View } from '../../components/Themed';
 import InputHour from '../../components/leaf/InputHour';
+import ArrowButton from '../../components/leaf/ArrowButton';
 
 export default function TabOneScreen() {
+  // TODO: Alertの動作を実装後に削除（テスト用に記載）
+  const onPressAlert = () => {
+    Alert.alert('アローボタンが押下されました。', '', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  };
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps='handled' scrollEnabled={false}>
       <View style={styles.container}>
         <InputHour label='Time' />
+        <ArrowButton direction='right' onClick={onPressAlert} />
+        <ArrowButton direction='left' onClick={onPressAlert} />
       </View>
     </ScrollView>
   );
