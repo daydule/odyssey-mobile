05cdee078a3d04e3edec1b66ad9d2f7929e8cc93
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-01-07 13:19:38 +0900

    ODY-260 add Header component

--- constants/Images.ts
+++ constants/Images.ts
@@ -2,6 +2,7 @@ import { ImageSourcePropType } from 'react-native';
 
 const Images: Record<string, ImageSourcePropType> = {
   icon_negate: require('../assets/images/icon_negate.jpeg'),
+  header_logo: require('../assets/images/header_logo.png')
 };
 
 export default Images;
