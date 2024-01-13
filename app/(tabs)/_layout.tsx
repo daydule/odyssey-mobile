595d25b58fec2e552b43e7e152d04590b8fc2ce3
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-01-13 10:14:14 +0900

    ODY-260 modify uncecessary variable declaration

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,8 +5,6 @@ import { Pressable, useColorScheme } from 'react-native';
 import Colors from '../../constants/Colors';
 import { Header } from '../../components/leaf/Header';
 
-const HEADER_TITLE = 'Time is Money';
-
 const TabBarIcon = (props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) => {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 };
