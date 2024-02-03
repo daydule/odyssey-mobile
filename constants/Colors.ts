057f939ce1f60f3b64d862fb024030ea053be592
tom-takeru <tom.2461.ty@gmail.com>
2024-02-03 09:34:15 +0900

    Update dark theme colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,5 @@
 const tintColorLight = '#2f95dc';
-// const tintColorDark = '#fff';
+const tintColorDark = '#fff';
 
 export default {
   light: {
@@ -10,15 +10,15 @@ export default {
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
     // text: '#000',
-    // background: '#000',
-    // tint: tintColorDark,
+    // background: '#fff',
+    // tint: tintColorLight,
     // tabIconDefault: '#ccc',
-    // tabIconSelected: tintColorDark,
+    // tabIconSelected: tintColorLight,
+    text: '#000',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
   },
 };
