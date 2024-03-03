7a57f06c14086f606417d0c9fbe5d1cc9747d2fa
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-03-03 17:18:00 +0900

    ODY-385 modify Aboutページのコンテンツの実装およびデザインの微修正

--- constants/Images.ts
+++ constants/Images.ts
@@ -3,7 +3,11 @@ import { ImageSourcePropType } from 'react-native';
 const Images: Record<string, ImageSourcePropType> = {
   iconNegate: require('../assets/images/icon_negate.jpeg'),
   aboutBackground: require('../assets/images/about_background_image.png'),
+  aboutContentLeftFront: require('../assets/images/about_left_card_front.png'),
+  aboutContentRightFront: require('../assets/images/about_right_card_front.png'),
+  aboutContentRightBack: require('../assets/images/about_right_card_back.png'),
   headerLogo: require('../assets/images/header_logo.png'),
+
 };
 
 export default Images;
